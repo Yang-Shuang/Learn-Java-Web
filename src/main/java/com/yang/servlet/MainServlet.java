763b2a9888da87cdb8f2a60af864f8ae4b294448
yangshuang <yangshuang@eagle-realestate.com>
2017-03-16 17:35:15 +0800

    测试servlet

--- src/main/java/com/yang/servlet/MainServlet.java
+++ src/main/java/com/yang/servlet/MainServlet.java
@@ -2,10 +2,28 @@ package com.yang.servlet;
 
 import com.yang.base.BaseServlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 
 /**
  * Created by Yang
  * on 2017/3/16.
  */
 public class MainServlet extends BaseServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.print("Succeeded");
+        writer.flush();
+        writer.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
 }
