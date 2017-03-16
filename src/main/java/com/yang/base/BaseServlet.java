06c0f0ed2dc1686f76609d9798ded27817460ef1
yangshuang <yangshuang@eagle-realestate.com>
2017-03-16 17:25:34 +0800

    目录结构

---
+++ src/main/java/com/yang/base/BaseServlet.java
@@ -0,0 +1,10 @@
+package com.yang.base;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by Yang
+ * on 2017/3/16.
+ */
+public class BaseServlet extends HttpServlet{
+}
---
+++ src/main/java/com/yang/servlet/MainServlet.java
@@ -0,0 +1,11 @@
+package com.yang.servlet;
+
+import com.yang.base.BaseServlet;
+
+
+/**
+ * Created by Yang
+ * on 2017/3/16.
+ */
+public class MainServlet extends BaseServlet {
+}
